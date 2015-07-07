cdaaac62828b3b003ed079d50cd166dae6cd7750
JohnAgapeyev <jagapeyev@gmail.com>
2015-07-06 20:44:11 -0700

    Adding eclipse work files

---
+++ src/conway/life/LifePanel.java
@@ -0,0 +1,22 @@
+package conway.life;
+
+import javax.swing.JPanel;
+
+public class LifePanel extends JPanel {
+    /**
+     * Serial ID for the panel.
+     */
+    private static final long serialVersionUID = 154602988047161157L;
+    
+    public LifePanel() {
+        
+    }
+    
+    
+    
+    
+
+    public static void main(String[] args) {
+        new LifePanel();
+    }
+}
